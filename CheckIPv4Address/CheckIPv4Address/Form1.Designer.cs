3be69cc7c46443eef49ad1ae90aec77165e26b5c
AlanaMarie <panny.alana@gmail.com>
2015-12-15 21:11:58 +0100

    Added new Check IPv4-Address solution

---
+++ CheckIPv4Address/CheckIPv4Address/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CheckIPv4Address
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIpAddress.Location = new System.Drawing.Point(12, 103);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(461, 93);
+            this.txtIpAddress.TabIndex = 11;
+            this.txtIpAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtIpAddress.TextChanged += new System.EventHandler(this.txtIpAddress_TextChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.White;
+            this.lblResult.Location = new System.Drawing.Point(9, 224);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(464, 239);
+            this.lblResult.TabIndex = 10;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(8, 59);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(247, 21);
+            this.lblDescription.TabIndex = 9;
+            this.lblDescription.Text = "Check if an IPv4-address is correct";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(5, 10);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(265, 37);
+            this.lblTitle.TabIndex = 8;
+            this.lblTitle.Text = "IPv4-address checker";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(487, 478);
+            this.Controls.Add(this.txtIpAddress);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ CheckIPv4Address/CheckIPv4Address/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckIPv4Address
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (txtIpAddress.ToString() == string.Empty)
+                lblResult.BackColor = Color.White;
+        }
+
+        private void txtIpAddress_TextChanged(object sender, EventArgs e)
+        {
+            String ipAddress;
+
+            ipAddress = txtIpAddress.ToString();
+
+            if (Validation(ipAddress))
+            {
+                lblResult.BackColor = Color.ForestGreen;
+                lblResult.Text = "IPv4 address is legit";
+
+            }
+
+            else
+            {
+                lblResult.BackColor = Color.Firebrick;
+                lblResult.Text = "IPv4 address is wrong";
+            }
+        }
+       
+        private bool Validation(string ipAddress)
+        {
+            Regex validationIpAddress = new Regex(@"(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])");
+
+            bool isIpAddress = false;
+
+            isIpAddress = validationIpAddress.IsMatch(ipAddress);
+
+            return isIpAddress;
+        }
+
+    }
+}
